e461df1780a6dfee0ddabb22b0bbc4add0121de5
Aaron Voymas <avoymas01@gmail.com>
2020-10-28 22:05:30 -0400

    created base wpf form for the xml editor.\nadded file selection capability.\nadded xmlparsing to treeview.

---
+++ XmlEditorTool/XMLEditorView.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XmlEditorTool
+{
+    /// <summary>
+    /// Interaction logic for XMLEditorView.xaml
+    /// </summary>
+    public partial class XMLEditorView : Page
+    {
+        public XMLEditorView()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadDrop(object sender, DragEventArgs e)
+        {
+            if (XmlTreeView.IsVisible)
+            {
+                XmlTreeView.Visibility = Visibility.Hidden;
+            }
+            // Make sure the file is an xml file
+            String[] format = e.Data.GetFormats(false);
+            bool validFormat = false;
+            foreach (String s in format)
+            {
+                if (s.Contains(".xml"))
+                {
+                    validFormat = true;
+                }
+            }
+
+            // only load the information if it is an xml and there is data present
+            if (validFormat && e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                XMLService.LoadTreeViewFromXmlFile(files[0], XmlTreeView);
+                XmlTreeView.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UploadFileClick(object sender, RoutedEventArgs e)
+        {
+            if (XmlTreeView.IsVisible)
+            {
+                XmlTreeView.Visibility = Visibility.Hidden;
+            }
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "eXtensible Markup Language (*.xml)|*.xml";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                XMLService.BuildTree(filename, XmlTreeView);
+                XmlTreeView.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ XmlEditorTool/XMLService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlEditorTool
+{
+    class XMLService
+    {
+        public static void LoadTreeViewFromXmlFile(string filename, System.Windows.Controls.TreeView view)
+        {
+            //Load the XML Document
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filename);
+
+            // Add the root node's children to the TreeView
+            view.Items.Clear();
+            System.Windows.Forms.TreeView treeView = new System.Windows.Forms.TreeView();
+            AddTreeViewChildNodes(treeView.Nodes, xmlDoc.DocumentElement);
+            view.DataContext = treeView;
+        }
+
+        private static void AddTreeViewChildNodes(
+            TreeNodeCollection parentNodes, XmlNode xmlNode)
+        {
+            foreach (XmlNode childNode in xmlNode.ChildNodes)
+            {
+                // Make the new TreeView node
+                TreeNode newNode = parentNodes.Add(childNode.Name);
+
+                // Recursively make this node's descendants
+                AddTreeViewChildNodes(newNode.Nodes, childNode);
+
+                // If this is a leaf node, make sure it's visible
+                if (newNode.Nodes.Count == 0) newNode.EnsureVisible();
+            }
+        }
+
+        public static void LoadTreeViewItemsFromXmlFile(string filename, System.Windows.Controls.TreeView view)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filename);
+
+            view.Items.Clear();
+            AddTreeViewItemNodes(view.Items, xmlDoc.DocumentElement);
+        }
+
+        private static void AddTreeViewItemNodes(
+            ItemCollection parentItems, XmlNode xmlNode)
+        {
+            foreach (XmlNode childNode in xmlNode.ChildNodes)
+            {
+                // Make the TreeViewItem
+                XmlNode node = (XmlNode)parentItems[parentItems.Add(childNode.Name)];
+                TreeViewItem newItem = new TreeViewItem();
+                newItem.DataContext = node;
+
+                // Make the descendants of the TreeViewItem
+                AddTreeViewItemNodes(newItem.Items, childNode);
+
+                // If this is a leaf item, make sure it's visible
+                if (newItem.Items.Count < 1)
+                {
+                    newItem.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        public static void BuildTree(String filepath, System.Windows.Controls.TreeView treeView)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filepath);
+            TreeViewItem treeNode = new TreeViewItem
+            {
+                //Should be Root
+                Header = doc.DocumentElement.Name,
+                IsExpanded = true
+            };
+            treeView.Items.Add(treeNode);
+            BuildNodes(treeNode, doc.DocumentElement);
+        }
+
+        private static void BuildNodes(TreeViewItem treeNode, XmlElement element)
+        {
+            foreach (XmlNode child in element.ChildNodes)
+            {
+                switch (child.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        XmlElement childElement = child as XmlElement;
+                        TreeViewItem childTreeNode = new TreeViewItem
+                        {
+                            //Get First attribute where it is equal to value
+                            Header = childElement.Name,//.Attributes().First(s => s.Name == "value").Value,
+                            //DataContext = childElement,
+                            //Automatically expand elements
+                            IsExpanded = true
+                        };
+                        treeNode.Items.Add(childTreeNode);
+                        BuildNodes(childTreeNode, childElement);
+                        break;
+                    case XmlNodeType.Text:
+                        XmlText childText = child as XmlText;
+                        treeNode.Items.Add(new TreeViewItem { Header = childText.Value, });
+                        break;
+                }
+            }
+        }
+    }
+}
