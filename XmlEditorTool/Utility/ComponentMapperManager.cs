6a63c2ee4175ad72fdf15b85694b674cd6aa224e
Aaron Voymas <avoymas01@gmail.com>
2020-11-01 00:59:26 -0400

    created a manager class to handle the component to source file mapper.

---
+++ XmlEditorTool/Utility/ComponentMapperManager.cs
@@ -0,0 +1,88 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmlEditorTool.Properties;
+
+namespace XmlEditorTool.Utility
+{
+    class ComponentMapperManager
+    {
+        private static ComponentMapperManager singleton;
+        public static ComponentMapperManager GetInstance()
+        {
+            if (singleton == null)
+            {
+                singleton = new ComponentMapperManager();
+            }
+            return singleton;
+        }
+
+        private ComponentMapperManager()
+        {
+            ReloadMapping();
+        }
+
+        private Dictionary<string,string> ComponentSourceFileMap;
+        public Dictionary<string,string> ComponentSourceFileMapProperty { get => ComponentSourceFileMap; set => ComponentSourceFileMap = value; }
+
+        // TODO -- replace this with a reference to preferences settings
+        private const string RESOURCE_FOLDER = "Resources/";
+        private const string COMPONENT_SOURCE_MAP_FILE = "ComponentToSrcFileMap.csv";
+
+        private void ReloadMapping()
+        {
+            ComponentSourceFileMapProperty = null;
+            CreateComponentSourceMapping();
+        }
+
+        private void CreateComponentSourceMapping()
+        {
+            /*
+             * load the csv file
+             * parse the contents with a csv parser, into the dictionary
+             */
+            // TODO -- change the const string reference to use a settings value
+            using (var stream = new StreamReader(Resources.ComponentToSourceFileMap))
+            using (var csv = new CsvReader(stream, CultureInfo.InvariantCulture))
+            {
+                using (var dr = new CsvDataReader(csv))
+                {
+                    var dt = new DataTable();
+                    dt.Columns.Add("Component", typeof(string));
+                    dt.Columns.Add("SourceFile", typeof(string));
+
+                    dt.Load(dr);
+
+                    ComponentSourceFileMapProperty = dt.AsEnumerable().ToDictionary<DataRow, string, string>(row => row.Field<string>(0),
+                        row => row.Field<string>(1));
+                }
+            }
+
+        }
+
+        private string SearchPropertyMapByKey(string key)
+        {
+            string msg = "No associated source file mapped";
+            try
+            {
+                return ComponentSourceFileMapProperty[key];
+            }
+            catch (KeyNotFoundException)
+            {
+                return msg;
+            }
+        }
+
+        public string GetSourceFile(string component)
+        {
+            return SearchPropertyMapByKey(component);
+        }
+    }
+}
