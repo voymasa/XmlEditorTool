820f371c19c21c9f61b1a498a4a4de757c89099c
Aaron Voymas <avoymas01@gmail.com>
2020-11-10 23:19:53 -0500

    misc files that escaped last push.

---
+++ XmlEditorTool/Utility/ComponentData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlEditorTool.Utility
+{
+    class ComponentData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string attributename;
+        private string datatype;
+        private object contentValue;
+
+        public string AttributeName
+        {
+            get { return attributename; }
+            set
+            {
+                attributename = value;
+                OnPropertyChanged("AttributeName");
+            }
+        }
+
+        public string Datatype
+        {
+            get { return datatype; }
+            set
+            {
+                datatype = value;
+                OnPropertyChanged("Datatype");
+            }
+        }
+
+        public object ContentValue
+        {
+            get { return contentValue; }
+            set
+            {
+                contentValue = value;
+                OnPropertyChanged("ContentValue");
+            }
+        }
+
+        public void OnPropertyChanged(string propertyname)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null) this.PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
