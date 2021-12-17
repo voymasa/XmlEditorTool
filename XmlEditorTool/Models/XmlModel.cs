5c87b77fb31e252608dd012b00032b202fa7a32e
voymasa <avoymas01@gmail.com>
2021-12-16 21:18:51 -0500

    #31 - updated the xml model and added a viewemodel list to it

--- XmlEditorTool/Models/XmlModel.cs
+++ XmlEditorTool/Models/XmlModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using XmlEditorTool.ViewModels;
 
 namespace XmlEditorTool.Models
 {
@@ -12,7 +13,8 @@ namespace XmlEditorTool.Models
         public string XmlFilePath { get; set; }
         public XmlDocument Document { get; private set; }
         public List<XmlElement> XmlElement { get; private set; }
-        public Dictionary<string, string> XmlMap { get; set; }
+        public List<XmlElementViewModel> ViewModels { get; protected set; }
+        //public Dictionary<string, string> XmlMap { get; set; }
         public XmlElement SelectedElement { get; set; }
         
         /*
@@ -21,10 +23,11 @@ namespace XmlEditorTool.Models
          */
         public XmlModel(string filepath)
         {
-            this.XmlFilePath = filepath;
-            this.Document = new XmlDocument();
+            XmlFilePath = filepath;
+            Document = new XmlDocument();
             Document.Load(filepath);
             XmlElement = new List<XmlElement>();
+            ViewModels = new List<XmlElementViewModel>();
         }
     }
 }
